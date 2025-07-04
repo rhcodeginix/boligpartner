d9925c797ed3beaa9c4d11b4ede3a141dd99e9a3
Drashti Savani <rhshukla124@gmail.com>
2025-07-04 12:39:00 +0530

    add some change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}", // Ensure this includes your React files
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
     colors: {
@@ -40,30 +38,22 @@ module.exports = {
     },
     screens: {
       sm: "576px",
-      // => @media (min-width: 576px) { ... }
       md: "768px",
-      // => @media (min-width: 768px) { ... }
       lg: "992px",
-      // => @media (min-width: 992px) { ... }
       laptop: "1024px",
-      // => @media (min-width: 1024px) { ... }
       desktop: "1280px",
-      // => @media (min-width: 1280px) { ... }
       big: "1440px",
-      // => @media (min-width: 1440px) { ... }
       sBig: "1600px",
-      // => @media (min-width: 1440px) { ... }
       xBig: "1920px",
-      // => @media (min-width: 1920px) { ... }
     },
     keyframes: {
       progress: {
-        '0%': { transform: 'translateX(-100%)' },
-        '100%': { transform: 'translateX(100%)' },
+        "0%": { transform: "translateX(-100%)" },
+        "100%": { transform: "translateX(100%)" },
       },
     },
     animation: {
-      progress: 'progress 1.5s linear infinite',
+      progress: "progress 1.5s linear infinite",
     },
   },
   plugins: [],
