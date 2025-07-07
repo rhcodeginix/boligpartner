980738990e44ea92daf659a5963b1c1fdcf9af5d
Drashti Savani <rhshukla124@gmail.com>
2025-07-07 11:06:21 +0530

    add some change

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,8 +1,23 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useIsAuthenticated } from "../hooks/useAuth";
+import { useEffect } from "react";
+import { analytics } from "../config/firebaseConfig";
+import { logEvent } from "firebase/analytics";
 
 export const AuthLayout = () => {
   const isAuthenticated = useIsAuthenticated();
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (analytics) {
+      logEvent(analytics, "page_view", {
+        page_path: location.pathname + location.search,
+        page_location: window.location.href,
+        page_title: document.title,
+      });
+    }
+  }, [location]);
 
   if (isAuthenticated) {
     return <Navigate to={"/Husmodell"} replace />;
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,26 @@
-import { Suspense } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Suspense, useEffect } from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { Spinner } from "../components/Spinner";
 import { Navbar } from "../components/Navbar";
 import ScrollToTop from "../components/common/scrollToTop";
 import { useIsAuthenticated } from "../hooks/useAuth";
+import { analytics } from "../config/firebaseConfig";
+import { logEvent } from "firebase/analytics";
 
 export const Layout = () => {
   const isAuthenticated = useIsAuthenticated();
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (analytics) {
+      logEvent(analytics, "page_view", {
+        page_path: location.pathname + location.search,
+        page_location: window.location.href,
+        page_title: document.title,
+      });
+    }
+  }, [location]);
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
