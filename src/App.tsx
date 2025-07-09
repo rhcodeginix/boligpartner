65777b8d1f7288e7f018c8cb1adf820fc9471ded
Drashti Savani <rhshukla124@gmail.com>
2025-07-09 17:48:40 +0530

    add some chanhge

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
 import "./App.css";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,6 +15,31 @@ const queryClient = new QueryClient({
 });
 
 export const App = () => {
+  useEffect(() => {
+    const handleTab = (e: any) => {
+      if (e.key === "Tab") {
+        e.preventDefault();
+
+        const focusable = Array.from(
+          document.querySelectorAll(
+            'input, select, textarea, button, a[href], [tabindex]:not([tabindex="-1"])'
+          )
+        ).filter((el: any) => !el.disabled && el.offsetParent !== null);
+
+        const index = focusable.indexOf((document as any).activeElement);
+        const next = e.shiftKey ? index - 1 : index + 1;
+
+        if (focusable.length > 0) {
+          const nextElement: any =
+            focusable[(next + focusable.length) % focusable.length];
+          nextElement.focus();
+        }
+      }
+    };
+
+    document.addEventListener("keydown", handleTab);
+    return () => document.removeEventListener("keydown", handleTab);
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster
