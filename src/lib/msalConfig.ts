108466f08955077ef1d21c9ef1f26b403c23f97a
Drashti Savani <rhshukla124@gmail.com>
2025-07-31 16:35:55 +0530

    add microsoft

--- src/lib/msalConfig.ts
+++ src/lib/msalConfig.ts
@@ -4,6 +4,6 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: `${process.env.REACT_APP_MICROSOFT_CLIENT_ID}`,
     authority: `https://login.microsoftonline.com/${process.env.REACT_APP_MICROSOFT_TENANT_ID}`,
-    redirectUri: "https://boligkonfigurator.mintomt.no/",
+    redirectUri: "https://boligkonfigurator.mintomt.no/auth/callback",
   },
 };
