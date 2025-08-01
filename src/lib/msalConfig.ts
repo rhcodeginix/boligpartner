deb635e5253a4f8c5b0ae7fa874e9b5a83a393f8
Drashti Savani <rhshukla124@gmail.com>
2025-08-01 11:14:52 +0530

    add some change

--- src/lib/msalConfig.ts
+++ src/lib/msalConfig.ts
@@ -3,6 +3,6 @@ export const msalConfig: any = {
     clientId: `${process.env.REACT_APP_MICROSOFT_CLIENT_ID}`,
     authority: `https://login.microsoftonline.com/${process.env.REACT_APP_MICROSOFT_TENANT_ID}`,
     redirectUri: "https://boligkonfigurator.mintomt.no/auth/callback",
-    clientSecret: "d3881780-ec21-4384-b782-ca44e48ab076",
+    clientSecret: `${process.env.REACT_APP_MICROSOFT_CLIENT_SECRET}`,
   },
 };
