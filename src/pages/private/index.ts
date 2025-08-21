ae119a9848795375d14ad07ace5a35889f641744
Drashti Savani <rhshukla124@gmail.com>
2025-08-21 15:27:31 +0530

    add new functionality

--- src/pages/private/index.ts
+++ src/pages/private/index.ts
@@ -1,6 +1,4 @@
-export { Inventory } from "./Inventory";
 export { Husmodeller } from "./Husmodeller";
 export { EditHouseModel } from "./editHusmodel";
 export { BoligConfiurator } from "./configurator";
 export { BoligConfiuratorStepper } from "./configurator/stepperPage";
-export { KundeInfo } from "./kundeInfo";
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import { Layout } from "./layouts";
 import {
-  Inventory,
   EditHouseModel,
   Husmodeller,
   BoligConfiurator,
@@ -30,7 +29,6 @@ export const routes = createBrowserRouter([
       { path: "Bolig-configurator", element: <BoligConfiurator /> },
       { path: "Room-Configurator", element: <BoligConfiuratorStepper /> },
       { path: "Room-Configurator/*", element: <BoligConfiuratorStepper /> },
-      { path: "Inventory", element: <Inventory /> },
       { path: "Husmodell", element: <Husmodeller /> },
       {
         path: "se-series/:seriesId/edit-husmodell/*",
